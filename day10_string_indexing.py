a0edfb28321877709b93b1515bb7b1662170439c
anushkayadav-ai <yadav.anushka1307@gmail.com>
2026-06-18 14:49:22 +0530

    Day 10: Learn string indexing and slicing
    A Python practice program that demonstrates string indexing and slicing techniques. The program accesses specific characters, extracts portions of a string, uses negative indexing, and reverses a credit card number using slicing. This project was created to practice working with sequences and understanding how indexing works in Python.

---
+++ day10_string_indexing.py
@@ -0,0 +1,14 @@
+# indexing = accessing elements of a sequence using [] (indexing operator)
+#            [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+last_digits = credit_number[-4:]
+
+# print(credit_number[10])
+# print(credit_number[:4])
+# print(credit_number[5:9])
+# print(credit_number[5:])
+# print(credit_number[-8])
+# print(credit_number[::9])
+print(f"XXXX-XXXX-XXXX-{last_digits}")
