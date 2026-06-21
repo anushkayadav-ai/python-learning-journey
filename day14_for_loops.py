6544d43189823a91abb69cdd554fd5d38b7a0608
anushkayadav-ai <yadav.anushka1307@gmail.com>
2026-06-21 15:19:18 +0530

    Day 14: Learn for loops in Python
    A Python practice program that demonstrates the use of for loops. The program iterates through ranges and strings, explores loop control statements such as continue and break, and uses reversed() with range(). This project was created to practice repetition, sequence traversal, and loop control in Python.

---
+++ day14_for_loops.py
@@ -0,0 +1,19 @@
+# for loops = execute a block of code a fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+credit_card = "1234-7893-6789-2387"
+# for x in credit_card:
+   
+# for x in reversed(range(1, 11, 3)):
+for x in range(1, 21):
+    if x == 13: 
+      #   break
+      continue 
+    else:
+        print(x)
+
+
+    
+   
+
+
