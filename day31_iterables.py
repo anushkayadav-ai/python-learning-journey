af9865b8d48a7872f76bf7dd5b139690f9596c5c
anushkayadav-ai <yadav.anushka1307@gmail.com>
2026-07-09 18:32:25 +0530

    Day 31: Learn iterables and looping through collections
    A Python practice program demonstrating how to iterate through different iterable objects, including tuples, sets, strings, and dictionaries. This project explores Python's iterable data types and reinforces the use of for loops to access elements efficiently.

---
+++ day31_iterables.py
@@ -0,0 +1,24 @@
+# Iterables = An object/colllection that can return its elements one at a time,
+#             allowing it to be iterated over in a loop
+
+numbers = (1, 2, 3, 4, 5)
+
+for number in numbers:
+    print(number)
+
+
+fruits = {"apple", "orange", "blueberry", "mango"}  # set object is not reversible
+
+for fruit in fruits:
+    print(fruit)
+
+
+name = "Anushka Yadav"
+
+for character in name:
+    print(character, end=" ")
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+for key, value in my_dictionary.items():
+    print(f"{key} = {value}")
